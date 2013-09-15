a1a458d25f8b1892bb22634c2334f506b9128ca1
Marton Kiss <marton.kiss@gmail.com>
2013-09-15 11:27:36 +0200

    Include translations in build-process
    Download commons translation files during make process, enable and
    import languages during installation, set language negotiation to
    URL based. Supported languages: zh-hant,zh-hans,ja,ko,vi,ru,pl,
    de,fr,es,it,pt-pt,hu,ar,tr
    
    Change-Id: I8f05a63b00f657225d3b8b84d531940dddbe8162

---
+++ scripts/build-translation-make.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+languages=(zh-hant zh-hans ja ko vi ru pl de fr es it pt-pt hu ar tr)
+
+echo "
+api = 2
+core = 7.x
+
+; Notice: we are using 'profile' as a project type here, because drush make
+; doesn't support direct download of contribution language files
+;
+"
+
+for i in "${languages[@]}"; do
+echo "
+projects[translation_${i}][type] = \"profile\"
+projects[translation_${i}][directory_name] = \"groups/translations\"
+projects[translation_${i}][download][type] = \"file\"
+projects[translation_${i}][download][url] = \"http://ftp.drupal.org/files/translations/7.x/commons/commons-7.x-3.3-rc2.${i}.po\""
+done
