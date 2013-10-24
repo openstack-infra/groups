2809cc17175d9e6210178e6e96bbf63af0dceca5
Marton Kiss <marton.kiss@gmail.com>
2013-10-24 22:09:02 +0200

    Openstack theme footer block
    Add openstack.org like footer block to public pages.
    
    Change-Id: Ie8f5d60f2efc19beb647d6c4a2499d8b716cd725

---
+++ modules/groups/groups_footer/templates/groups_footer.tpl.php
@@ -0,0 +1,48 @@
+    <hr>
+    <div class="span-8">
+      <h3>My Feedback About This Page</h3>
+<form enctype="application/x-www-form-urlencoded" method="post" action="/software/FeedbackForm" id="FeedbackForm_FeedbackForm">
+  <textarea cols="20" rows="5" name="Content" id="FeedbackForm_FeedbackForm_Content"></textarea>
+  <input type="submit" title="Tell Us" value="Tell Us" name="action_submitFeedback" id="FeedbackForm_FeedbackForm_action_submitFeedback" class="action  button">
+</form>
+
+    </div>
+    <div class="span-4">
+      <h3>OpenStack</h3>
+      <ul>
+        <li><a href="http://openstack.org/projects/">Projects</a></li>
+        <li><a href="http://openstack.org/openstack-security/">OpenStack Security</a></li>
+        <li><a href="http://openstack.org/projects/openstack-faq/">Common Questions</a></li>
+        <li><a href="http://openstack.org/blog/">Blog</a></li>
+      </ul>
+    </div>
+    <div class="span-4">
+      <h3>Community</h3>
+      <ul>
+        <li><a href="http://openstack.org/community/">User Groups</a></li>
+        <li><a href="http://openstack.org/events/">Events</a></li>
+        <li><a href="http://openstack.org/jobs/">Jobs</a></li>
+        <li><a href="http://openstack.org/companies/">Companies</a></li>
+        <li><a href="http://wiki.openstack.org/HowToContribute">Contribute</a></li>
+      </ul>
+    </div>
+    <div class="span-4">
+      <h3>Documentation</h3>
+      <ul>
+        <li><a href="http://docs.openstack.org/">OpenStack Manuals</a></li>
+        <li><a href="http://www.openstack.org/software/start/">Getting Started</a></li>
+        <li><a href="http://api.openstack.org/">API Documentation</a></li>
+        <li><a href="http://wiki.openstack.org/">Wiki</a></li>
+      </ul>
+    </div>
+    <div class="span-4 last">
+      <h3>Branding &amp; Legal</h3>
+      <ul>
+        <li><a href="http://openstack.org/brand/">Logos &amp; Guidelines</a></li>
+        <li><a href="http://openstack.org/brand/openstack-trademark-policy/">Trademark Policy</a></li>
+        <li><a href="http://openstack.org/privacy/">Privacy Policy</a></li>
+        <li><a href="http://wiki.openstack.org/CLA">OpenStack CLA</a></li>
+      </ul>
+    </div>
+    <hr>
+    <p>The OpenStack project is provided under the Apache 2.0 license. Openstack.org is powered by <a href="http://www.rackspacecloud.com/">Rackspace Cloud Computing</a>.</p>
