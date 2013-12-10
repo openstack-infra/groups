e7e0a499b74041131c1322f272f1fcac0ff8a11e
Marton Kiss <marton.kiss@gmail.com>
2013-12-10 21:30:02 +0100

    Frontpage block layout
    Reorganize frontpage layout and add new block templates like
    community stats, community map and find nearby groups.
    
    Change-Id: I1acf920113068ba8cba8af78cf2e532002c569cd

---
+++ modules/groups/groups_homepage/templates/community_stats.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Template of community statistics block.
+ */
+?>
+<div>
+  <em><?php print $stats['people'];?></em> people in <em><?php print $stats['countries'];?></em> countries and <em><?php print $stats['user-groups'];?></em> established user groups all around the world.
+</div>
---
+++ modules/groups/groups_homepage/templates/groups_find_nearby.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template of front page groups finder block.
+ */
+?>
+<div>
+  <form>
+    <p>
+    Find the nearest usergroup in your region and meet with other stackers!
+    </p>
+    <input type="text" name="region"/>
+    <input type="submit" value="Find"/>
+  </form>
+  <h3>Not found?</h3>
+  <p>
+  Establish a new Openstack User Group in your city. Read our organizer tips About bootstrapping a local group and join to our fast growing vibrant community!
+  </p>
+  <a href="#">Register new user group</a>
+</div>
