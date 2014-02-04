86a75eec5822a2b02ea55019d529607887a3f429
Marton Kiss <marton.kiss@gmail.com>
2014-02-04 13:55:37 +0100

    Add dev-build.sh script
    Build distribution from local git repository.
    
    Change-Id: Idaddff40b0adf944fcd9180e1ebf1fbb8a5475e2

---
+++ scripts/dev-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Build groups distribution from local filesystem instead of fetching
+# directly from git
+#
+
+TARGET_DIR=publish
+
+drush make drupal-org-core.make $TARGET_DIR
+mkdir -p $TARGET_DIR/profiles/groups
+rsync -av --exclude='$TARGET_DIR' . $TARGET_DIR/profiles/groups/
+drush make --no-core --no-cache --contrib-destination=profiles/groups drupal-org.make $TARGET_DIR.contrib
+rsync -av $TARGET_DIR.contrib/* $TARGET_DIR/profiles/groups/
+rm -rf $TARGET_DIR.contrib
