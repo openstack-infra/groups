3dbea6ada1eed1bd5896d025a14654cc63e8b7f1
Marton Kiss <marton.kiss@gmail.com>
2014-02-04 14:51:44 +0100

    Dev build script update
    Fix some minor bugs that broke the build process.
    
    Change-Id: Ie29296fbc65d7ef5e74a8832474d42b32ef8bccd

--- scripts/dev-build.sh
+++ scripts/dev-build.sh
@@ -8,7 +8,7 @@ TARGET_DIR=publish
 
 drush make drupal-org-core.make $TARGET_DIR
 mkdir -p $TARGET_DIR/profiles/groups
-rsync -av --exclude='$TARGET_DIR' . $TARGET_DIR/profiles/groups/
+rsync -av --exclude=$TARGET_DIR . $TARGET_DIR/profiles/groups/
 drush make --no-core --no-cache --contrib-destination=profiles/groups drupal-org.make $TARGET_DIR.contrib
-rsync -av $TARGET_DIR.contrib/* $TARGET_DIR/profiles/groups/
+rsync -av $TARGET_DIR.contrib/* $TARGET_DIR/
 rm -rf $TARGET_DIR.contrib
