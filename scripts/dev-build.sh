de6621a9ce15d771f4a63f593e7c5646676bae0e
Marton Kiss <marton.kiss@gmail.com>
2014-06-30 16:03:57 +0200

    Add target-dir arg support to dev-build script
    This patch provide a custom TARGET_DIR feature to dev-build.sh script.
    When not specified, TARGET_DIR value defaults to publish. With this
    patch dev-build /your-custom-directory format is supported so
    distribution build destination directory can be different from
    publish.
    
    Change-Id: Icd561748b7909b8d114be311bee4118e0e582e20

--- scripts/dev-build.sh
+++ scripts/dev-build.sh
@@ -4,11 +4,16 @@
 # directly from git
 #
 
-TARGET_DIR=publish
+TARGET_DIR=${TARGET_DIR:-publish}
+PROFILE_NAME=${PROFILE_NAME:-groups}
+
+if [ ! -z "$1" ]; then
+  TARGET_DIR=$1
+fi
 
 drush make drupal-org-core.make $TARGET_DIR
-mkdir -p $TARGET_DIR/profiles/groups
-rsync -av --exclude=$TARGET_DIR --exclude=drush . $TARGET_DIR/profiles/groups/
-drush make --no-core --no-cache --contrib-destination=profiles/groups drupal-org.make $TARGET_DIR.contrib
+mkdir -p $TARGET_DIR/profiles/$PROFILE_NAME
+rsync -av --exclude=$TARGET_DIR --exclude=drush . $TARGET_DIR/profiles/$PROFILE_NAME/
+drush make --no-core --no-cache --contrib-destination=profiles/$PROFILE_NAME drupal-org.make $TARGET_DIR.contrib
 rsync -av $TARGET_DIR.contrib/* $TARGET_DIR/
 rm -rf $TARGET_DIR.contrib
