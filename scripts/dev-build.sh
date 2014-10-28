61c77f78c788d5e4bc1f84cae539d0ba23aed91b
Marton Kiss <marton.kiss@gmail.com>
2014-10-28 14:14:43 +0100

    Compile theme css as part of build process
    Install required gems and compile css from sass source, using
    bundle tool. Clean-up .bundle-gems and .sass-cache temporary
    dirs.
    
    Change-Id: I6cc43bfecc8e3f5a265d2bff9d20fc760f84f497

--- scripts/dev-build.sh
+++ scripts/dev-build.sh
@@ -6,6 +6,7 @@
 
 TARGET_DIR=${TARGET_DIR:-publish}
 PROFILE_NAME=${PROFILE_NAME:-groups}
+THEME_NAME=${THEME_NAME:-openstack_bootstrap}
 
 if [ ! -z "$1" ]; then
   TARGET_DIR=$1
@@ -17,3 +18,16 @@ rsync -av --exclude=$TARGET_DIR --exclude=drush . $TARGET_DIR/profiles/$PROFILE_
 drush make --no-core --no-cache --contrib-destination=profiles/$PROFILE_NAME drupal-org.make $TARGET_DIR.contrib
 rsync -av $TARGET_DIR.contrib/* $TARGET_DIR/
 rm -rf $TARGET_DIR.contrib
+
+# build theme css from sass files
+cwd=$(pwd)
+cd $TARGET_DIR/profiles/$PROFILE_NAME/themes/$THEME_NAME
+if [ -f Gemfile ]; then
+  mkdir .bundled_gems
+  export GEM_HOME=`pwd`/.bundled_gems
+  bundle install
+  bundle exec compass compile
+  # cleanup
+  rm -rf .bundled_gems .sass-cache
+fi
+cd $cwd
