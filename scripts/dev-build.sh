c0f936412275ab2e09405f64741caa7b8d9605bf
Marton Kiss <marton.kiss@gmail.com>
2014-10-28 14:34:17 +0100

    Disable verbose rsync in dev-build script
    Turn off verbose output in dev-build script to reduce log output
    of build process.
    
    Change-Id: Ib40cdf9e698b5120afb8e8eeea9de0ccbd8c2b4a

--- scripts/dev-build.sh
+++ scripts/dev-build.sh
@@ -14,9 +14,9 @@ fi
 
 drush make drupal-org-core.make $TARGET_DIR
 mkdir -p $TARGET_DIR/profiles/$PROFILE_NAME
-rsync -av --exclude=$TARGET_DIR --exclude=drush . $TARGET_DIR/profiles/$PROFILE_NAME/
+rsync -a --exclude=$TARGET_DIR --exclude=drush . $TARGET_DIR/profiles/$PROFILE_NAME/
 drush make --no-core --no-cache --contrib-destination=profiles/$PROFILE_NAME drupal-org.make $TARGET_DIR.contrib
-rsync -av $TARGET_DIR.contrib/* $TARGET_DIR/
+rsync -a $TARGET_DIR.contrib/* $TARGET_DIR/
 rm -rf $TARGET_DIR.contrib
 
 # build theme css from sass files
