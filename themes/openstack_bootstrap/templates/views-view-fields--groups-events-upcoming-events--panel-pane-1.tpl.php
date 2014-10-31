f4a3682a0483b0e2ea485da4162fd907ef2edb66
Marton Kiss <marton.kiss@gmail.com>
2014-10-31 22:12:53 +0100

    Event venue name on event form and upcoming event block
    Add venue name to the event content type and upcoming events block.
    
    Change-Id: Ib9d8b812d56560b00b69945d16f4ea17f36e94c8

--- themes/openstack_bootstrap/templates/views-view-fields--groups-events-upcoming-events--panel-pane-1.tpl.php
+++ themes/openstack_bootstrap/templates/views-view-fields--groups-events-upcoming-events--panel-pane-1.tpl.php
@@ -11,23 +11,16 @@
  */
 ?>
 
-<?php dpm($fields); ?>
-
 <div class="event--large-teaser">
   <div class="container-header">
     <h3><?php print $fields['title_field']->content; ?></h3>
   </div>
   <div class="container-first">
+    <div class="field-date"><?php print $fields['field_date']->content; ?></div>
+    <div class="field-venue"><?php print $fields['field_venue_name']->content; ?> (<?php print $fields['field_address']->content; ?>)</div>
     <div class="field-body"><?php print $fields['body']->content; ?></div>
   </div>
   <div class="container-second">
-    <div class="field-date"><?php
-      // TODO: rewrite this part into a custom date field formatter.
-      $date_str = $fields['field_date']->content;
-      $date_str = str_replace('<div class="field-content">', '<div class="field-content"><div class="date-part">', $date_str);
-      $date_str = str_replace(',', '</div>', $date_str);
-      print $date_str;
-    ?></div>
     <div class="field-capacity"><?php print $fields['capacity_used']->content; ?></div>
     <div class="field-comment-count"><?php print $fields['comment_count']->content; ?></div>
   </div>
