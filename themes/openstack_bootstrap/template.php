e9b0b89c35a659ecdd3f1bd6bb351b322dcd64cd
Marton Kiss <marton.kiss@gmail.com>
2014-12-29 16:39:42 +0100

    Enable content stream on groups page
    Display a stream of content on groups page and enable content
    post feature, based on user roles and group membership. Content
    creation allowed for group organizers only.
    
    Change-Id: Iebed964fd296fffde63a1f6e140d0ea2d548220d

--- themes/openstack_bootstrap/template.php
+++ themes/openstack_bootstrap/template.php
@@ -61,3 +61,112 @@ function openstack_bootstrap_links__commons_utility_links(&$variables) {
     return l($link['title'], $link['href'], $link);
   }
 }
+
+/**
+ * Implements hook_theme().
+ */
+function openstack_bootstrap_theme($existing, $type, $theme, $path) {
+  return array(
+    // Register the newly added theme_form_content() hook so we can utilize
+    // theme hook suggestions.
+    // @see commons_bootstrap_form_alter().
+    'form_content' => array(
+      'render element' => 'form',
+      'path' => drupal_get_path('theme', 'openstack_bootstrap') . '/templates',
+      'template' => 'form-content',
+      'pattern' => 'form_content__',
+    ),
+  );
+}
+
+/**
+ * Implements hook_form_alter().
+ */
+function openstack_bootstrap_form_alter(&$form, &$form_state, $form_id) {
+  // Give forms a common theme function so we do not have to declare every
+  // single form we want to override in hook_theme().
+  if (is_array($form['#theme'])) {
+    $hooks = array('form_content');
+    $form['#theme'] = array_merge($form['#theme'], $hooks);
+  }
+  else {
+    $form['#theme'] = array(
+      $form['#theme'],
+      'form_content',
+    );
+  }
+}
+
+/**
+ * Implements hook_preprocess_form_content().
+ */
+function openstack_bootstrap_preprocess_form_content(&$variables, $hook) {
+  // Bootstrap the with some of Drupal's default variables.
+  template_preprocess($variables, $hook);
+
+  if (strpos($variables['form']['#form_id'], 'commons_bw_partial_node_form_') === 0) {
+    $variables['form']['actions']['submit']['#attributes']['class'][] = 'action-item-primary';
+    if (isset($variables['form']['title'])) {
+      $variables['form']['title']['#markup'] = '<h3 class="partial-node-form-title">' . $variables['form']['title']['#markup'] . '</h3>';
+    }
+  }
+}
+
+/**
+ * Override or insert variables into the node templates.
+ */
+function openstack_bootstrap_preprocess_node(&$variables, $hook) {
+  $node = $variables['node'];
+  $wrapper = entity_metadata_wrapper('node', $node);
+  // Use timeago module for formatting node submission date
+  // if it is enabled and also configured to be used on nodes.
+  if (module_exists('timeago') && variable_get('timeago_node', 1)) {
+    $variables['date'] = timeago_format_date($node->created, $variables['date']);
+    $use_timeago_date_format = TRUE;
+  }
+  else {
+    $use_timeago_date_format = FALSE;
+  }
+  // If there does happen to be a user image, add a class for styling purposes.
+  if (!empty($variables['user_picture'])) {
+    $variables['classes_array'][] = 'user-picture-available';
+  }
+  // Replace the submitted text on nodes with something a bit more pertinent to
+  // the content type.
+  if (variable_get('node_submitted_' . $node->type, TRUE)) {
+    $node_type_info = node_type_get_type($variables['node']);
+    $type_attributes = array('class' => array(
+      'node-content-type',
+      drupal_html_class('node-content-type-' . $node->type),
+    ));
+    $placeholders = array(
+      '!type' => '<span' . drupal_attributes($type_attributes) . '>' . check_plain($node_type_info->name) . '</span>',
+      '!user' => $variables['name'],
+      '!date' => $variables['date'],
+      '@interval' => format_interval(REQUEST_TIME - $node->created),
+    );
+    // $node_user = user_load($node->uid);
+    // $placeholders['!user'] = format_username($node_user);
+    if (!empty($node->{OG_AUDIENCE_FIELD}) && $wrapper->{OG_AUDIENCE_FIELD}->count() == 1) {
+      $placeholders['!group'] = l($wrapper->{OG_AUDIENCE_FIELD}->get(0)->label(), 'node/' . $wrapper->{OG_AUDIENCE_FIELD}->get(0)->getIdentifier());
+      if ($use_timeago_date_format == TRUE) {
+        $variables['submitted'] = t('!type created !date in the !group group by !user', $placeholders);
+      }
+      else {
+        $variables['submitted'] = t('!type created @interval ago in the !group group by !user', $placeholders);
+      }
+    }
+    else {
+      if ($use_timeago_date_format == TRUE) {
+        $variables['submitted'] = t('!type created !date by !user', $placeholders);
+      }
+      else {
+        $variables['submitted'] = t('!type created @interval ago by !user', $placeholders);
+      }
+    }
+  }
+  // Notice: a not-so-nice hack to pass submitted variable to custom node.tpl.php, some module
+  // overwrites the original submitted variable, so we are passing submitted_ now.
+  // @see templates/node.tpl.php
+  $variables['submitted_'] = $variables['submitted'];
+}
