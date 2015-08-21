b967d017027dfad7bc6dabd0379ea4585e30332a
Marton Kiss <marton.kiss@gmail.com>
2015-08-21 12:35:34 +0200

    Add groups ambassador page
    The new page enlist the ambassadors and include a lead block for
    ambassador program faq. New assets defined by the patch:
    
    groups ambassador module: building blocks
      - profile teaser view mode for user entity
      - groups_ambassador_list view
      - ambassador roles block
      - ambassador selection process block
      - ambassador program faq block
    
    groups ambassador page module: landing pages
      - ambassador program main menu
      - ambassador program page
    
    Change-Id: I0e2d92b74c34baaff7aacb4055e9b6c2f1614e0a

---
+++ modules/groups/groups_ambassador/templates/groups_ambassador_faq.tpl.php
@@ -0,0 +1,24 @@
+<h3>When is all this going to happen?</h3>
+<p>We hope to have our first tranche of ambassadors entitled by September, so
+we can get the program running before the summit. We want to get this done as
+soon as manageable, since we expect it to go a long way to solving some
+problems that exist right now. </p>
+<h3>How much do ambassadors get paid?</h3>
+<p>hey are paid in love – this is a volunteer position.</p>
+<h3>How big will the regions be?</h3>
+<p>Initially: big. We’re starting with a small number of ambassadors so we can
+learn before scaling up. We also want to avoid situations where there are many
+ambassadors in one region – hence the selection criteria related to geography.
+</p>
+<h3>How does “Ambassador” relate to “User Group Leader”?</h3>
+
+<p>OpenStack Ambassador and OpenStack User Group leader are two distinct roles, which can of course be fulfilled by one person, but they have distinct responsibilities. The Ambassador may be involved in running their local meetup group, but they’ll be chatting to the User Group Leaders in their region about how to best help – not taking over!
+</p>
+<h3>What will the term length be for ambassadors?</h3>
+
+<p>It’s a ‘job for life’. We believe that the kinds of people who would be eligible to be ambassadors will know when to step down gracefully.
+</p>
+<h3>How will we manage misuse of the ambassador title?</h3>
+
+<p>This question comes up frequently, but we have faith in our community. We’ll be applying selection criteria to weed out any undesirables at the beginning, but then keeping in constant contact with the ambassadors to look for early signs things aren’t going the right way. Community members are also welcome to help us looking out for misuse of the title.
+</p>
---
+++ modules/groups/groups_ambassador/templates/groups_ambassador_selection.tpl.php
@@ -0,0 +1,21 @@
+<p>The title is designed to recognise those who are already good leaders
+with a proven track record in the community. With the title, new users,
+contributors and community members should easily be able to recognize their
+Ambassador as a go-to resource. While the role provides inherent
+credibility, it also comes with accountability, meaning decisions and
+actions made by the Ambassador should benefit the whole community, not just
+one person or company.</p>
+<p>We believe that the Ambassadors will already be active participants in our community. Think about those people who are already working across multiple user groups, submitting OpenStack mini-events to related conferences, helping onboard new users and contributors, arranging hackfests or just generally going above and beyond in the name of making Openstack great.
+</p>
+<p>To that end, we intend to ask that potential ambassadors provide some information about themselves, namely:
+</p>
+<ul><li> Why are you applying to become an OpenStack Ambassador?</li>
+<li> How have you participated in the OpenStack Community to date?</li>
+<li> What ideas do you have for your community, that you wish you had time or resources to implement?</li></ul>
+<p>A small number of ambassadors will then be selected based on a set of criteria:
+</p>
+<ul>
+  <li> Community participation track record</li>
+  <li> Geographical location</li>
+  <li> Potential impact of selection on community</li>
+</ul>
