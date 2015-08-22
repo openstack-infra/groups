59baa415843acb2742fa7b916682b31d58cf307a
Marton Kiss <marton.kiss@gmail.com>
2015-08-22 12:51:17 +0200

    Add social link to user profile on ambassador page
    Display the twitter account of ambassadors on the ambassador page.
    - groups_social_link formatter module for link_fields
    - fontawesome iconset to support font icons (CDN version)
    - update of user profiles's profile teaser display suite settings
    
    Change-Id: Ieba3b5ff047792625fdb23fb7ab498ee355e9dfc

--- themes/openstack_bootstrap/template.php
+++ themes/openstack_bootstrap/template.php
@@ -22,6 +22,7 @@ function openstack_bootstrap_preprocess_page(&$variables) {
     unset($variables['navbar_classes_array'][$key]);
     $variables['navbar_classes_array'][] = 'navbar-os';
   }
+  drupal_add_css('//netdna.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.css', array('type' => 'external'));
 }
 
 /**
