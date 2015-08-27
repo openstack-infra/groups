bc02052b0e465bb816037743be5e238d70f7b10d
Marton Kiss <marton.kiss@gmail.com>
2015-08-27 20:15:03 +0200

    Add ambassador application form
    Add an ambassador application form, registered users can apply
    to the position. The submit hook will save the registration form
    data to content management and invoke a notification message
    to ambassadors.
    
    Change-Id: Ia702ae80f44b1289c164dcca8e70f993b98c346f

---
+++ modules/groups/groups_ambassador/templates/groups_ambassador_apply_confirm.tpl.php
@@ -0,0 +1,4 @@
+<div class="ask-form-thankyou">
+  <p>Thank you for your Ambassador application.</p>
+  <p class="ambassador-link"><a href="/ambassador-program"><i class="fa fa-chevron-left"></i> Return to Ambassador program page</a></p>
+</div>
