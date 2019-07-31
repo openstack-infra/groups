d8dbe94b983e914a988a30230590b6864578f573
Monty Taylor <mordred@inaugust.com>
2019-07-31 13:31:08 -0400

    Retire github mirror, repo moved to opendev

--- modules/groups/groups_ambassador/templates/groups_ambassador_submission.tpl.php
+++
@@ -1,5 +0,0 @@
-<div class="ask-form-thankyou">
-  <p>Thank you for your question, our Ambassadors will reply soon.<br/>Your
-  comments and suggestions are invaluable for our community.</p>
-  <p class="ambassador-link"><a href="/ambassador-program"><i class="fa fa-chevron-left"></i> Return to Ambassador program page</a></p>
-</div>
--- themes/openstack_bootstrap/templates/block--locale.tpl.php
+++
@@ -1,9 +0,0 @@
-<section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix collapse"<?php print $attributes; ?>>
-
-  <?php print render($title_prefix); ?>
-  <h2><?php print t('Select language'); ?></h2>
-  <?php print render($title_suffix); ?>
-
-  <?php print $content ?>
-
-</section> <!-- /.block -->
